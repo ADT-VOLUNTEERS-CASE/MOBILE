323d385fe83fb9845ef8753a5c9d5452d3c2d36e
freeze2222 <opop1094@gmail.com>
2025-12-17 20:30:14 +0300

    Add theming

--- presentation/src/main/kotlin/org/adt/presentation/theme/Color.kt
+++ presentation/src/main/kotlin/org/adt/presentation/theme/Color.kt
@@ -1,11 +1,37 @@
 package org.adt.presentation.theme
 
+import android.annotation.SuppressLint
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//Example values
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Golden = Color(0xFFCE9760)
+val LightBrown = Color(0xFFCE9760)
+val Brown = Color(0xFF543A20)
+
+@Immutable
+data class AppColors(
+    val primaryBackground: Color,
+    val secondaryBackground: Color,
+    val tertiaryBackground: Color,
+    val text: Color
+)
+
+@SuppressLint("ComposeCompositionLocalUsage")
+val LocalAppColors = staticCompositionLocalOf {
+    AppColors(
+        primaryBackground = Color.Unspecified,
+        secondaryBackground = Color.Unspecified,
+        tertiaryBackground = Color.Unspecified,
+        text = Color.Unspecified
+    )
+}
+
+val extendedColor = AppColors(
+    primaryBackground = Brown,
+    secondaryBackground = Golden,
+    tertiaryBackground = LightBrown,
+    text = Color.White
+)
