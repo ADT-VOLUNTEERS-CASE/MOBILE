1a01328c659b274e1d780e6d52956fb8567bf422
freeze2222 <opop1094@gmail.com>
2026-03-23 18:39:05 +0300

    Fix example instrumented test package assertion

--- storage/src/androidTest/java/org/adt/storage/ExampleInstrumentedTest.kt
+++ storage/src/androidTest/java/org/adt/storage/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.profs.storage.test", appContext.packageName)
+        assertEquals("org.adt.storage.test", appContext.packageName)
     }
 }
