fdcf8ede174cf8e4c6778d6b2250a172d4a3c828
freeze2222 <opop1094@gmail.com>
2026-04-03 15:17:06 +0300

    Add empty homeScreen preview for successful roborazzi generation.

--- presentation/src/main/kotlin/org/adt/presentation/screens/home/HomeScreen.kt
+++ presentation/src/main/kotlin/org/adt/presentation/screens/home/HomeScreen.kt
@@ -1,8 +1,15 @@
 package org.adt.presentation.screens.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import org.adt.presentation.theme.VolunteersCaseTheme
 
 @Composable
 fun HomeScreen(
@@ -14,11 +21,15 @@ fun HomeScreen(
 
 @Composable
 fun HomeScreenContent() {
-    //TODO
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Test", color = Color.White)
+    }
 }
 
 @Preview
 @Composable
 private fun HomeScreenPreview() {
-    HomeScreenContent()
+    VolunteersCaseTheme {
+        HomeScreenContent()
+    }
 }
