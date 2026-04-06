b947a1ef12420a0258843103473b271e02f947de
freeze2222 <opop1094@gmail.com>
2026-04-06 20:46:55 +0300

    Partial unit tests

--- data/src/test/kotlin/org/adt/data/repository/DataRepositoryTest.kt
+++ data/src/test/kotlin/org/adt/data/repository/DataRepositoryTest.kt
@@ -9,6 +9,7 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.ResponseBody.Companion.toResponseBody
 import org.adt.core.entities.UserRole
 import org.adt.core.entities.request.AuthRequest
+import org.adt.core.entities.request.RefreshRequest
 import org.adt.core.entities.request.RegisterRequest
 import org.adt.core.entities.response.AuthResponse
 import org.adt.data.abstraction.PersistenceRepository
@@ -17,6 +18,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import retrofit2.Response
+import kotlin.random.Random
 
 data class MockUserModel(
     val email: String = "", val password: String = "", val role: UserRole = UserRole.VOLUNTEER
@@ -26,17 +28,29 @@ class DataRepositoryTest {
     lateinit var retrofitMockRepository: RetrofitRepository
     lateinit var persistenceMockRepository: PersistenceRepository
 
-    lateinit var dataMockRepository: DataRepository
+    lateinit var dataRepository: DataRepository
 
     var usersList = mutableListOf<MockUserModel>()
     val exceptedPingResponse = "Pong!"
 
     val registerRequestSlot = slot<RegisterRequest>()
     val authRequestSlot = slot<AuthRequest>()
+    val refreshRequestSlot = slot<RefreshRequest>()
+
+    val accessTokenSlot = slot<String>()
+    val refreshTokenSlot = slot<String>()
+
+    var tokenStore = Pair("", "")
+    var refreshTokensStore = HashMap<String, MockUserModel>()
+    var accessTokensStore = HashMap<String, MockUserModel>()
 
     @Before
     fun setup() {
         usersList.clear()
+        tokenStore = Pair("", "")
+        refreshTokensStore.clear()
+        accessTokensStore.clear()
+
         retrofitMockRepository = mockk(relaxed = true) {
 
             coEvery { ping() } returns Response.success(exceptedPingResponse)
@@ -44,17 +58,25 @@ class DataRepositoryTest {
             // region Registration
 
             coEvery { registerVolunteer(request = capture(registerRequestSlot)) } answers {
-                addCapturedUserToList(UserRole.VOLUNTEER)
+                val user = addCapturedUserToListAndRetrieve(UserRole.VOLUNTEER)
+
+                val refreshToken = Random.nextBytes(128).toString()
+                refreshTokensStore[refreshToken] = user
+
+                Response.success(AuthResponse(refreshToken = refreshToken))
+            }
+
+            coEvery {
+                registerCoordinator(
+                    request = capture(registerRequestSlot), auth = ""
+                )
+            } answers {
+                addCapturedUserToListAndRetrieve(UserRole.COORDINATOR)
                 Response.success(AuthResponse())
             }
 
-            coEvery { registerCoordinator(request = capture(registerRequestSlot)) } answers {
-                addCapturedUserToList(UserRole.COORDINATOR)
-                Response.success(AuthResponse())
-            }
-
-            coEvery { registerAdmin(request = capture(registerRequestSlot)) } answers {
-                addCapturedUserToList(UserRole.ADMIN)
+            coEvery { registerAdmin(request = capture(registerRequestSlot), auth = "") } answers {
+                addCapturedUserToListAndRetrieve(UserRole.ADMIN)
                 Response.success(AuthResponse())
             }
 
@@ -62,11 +84,14 @@ class DataRepositoryTest {
 
             coEvery { authenticate(capture(authRequestSlot)) } answers {
                 val request = authRequestSlot.captured
-                val isSuccess = usersList.any { user ->
+                val user = usersList.filter { user ->
                     request.email == user.email && request.password == user.password
                 }
 
+                val isSuccess = user.isNotEmpty()
+
                 if (isSuccess) {
+
                     Response.success(AuthResponse())
                 } else {
                     Response.error(
@@ -75,55 +100,100 @@ class DataRepositoryTest {
                     )
                 }
             }
+
+            coEvery { refreshToken(capture(refreshRequestSlot)) } answers {
+                val request = refreshRequestSlot.captured
+
+                val user = refreshTokensStore.getOrDefault(request.refreshToken, MockUserModel())
+
+                if (user.email.isBlank()) {
+                    Response.error(
+                        -1,
+                        """{}""".toResponseBody("application/json".toMediaType())
+                    )
+                } else {
+                    val newAccessToken = Random.nextBytes(128).toString()
+                    val newRefreshToken = Random.nextBytes(128).toString()
+
+                    refreshTokensStore.remove(request.refreshToken)
+                    refreshTokensStore[newRefreshToken] = user
+
+                    Response.success(AuthResponse(newAccessToken, newRefreshToken))
+                }
+            }
         }
 
-        persistenceMockRepository = mockk(relaxed = true)
+        persistenceMockRepository = mockk(relaxed = true) {
+            coEvery {
+                saveTokens(
+                    capture(accessTokenSlot),
+                    capture(refreshTokenSlot),
+                )
+            } answers {
+                tokenStore = Pair(accessTokenSlot.captured, refreshTokenSlot.captured)
+            }
 
-        dataMockRepository = DataRepositoryImpl(retrofitMockRepository, persistenceMockRepository)
+            coEvery { getToken() } answers {
+                tokenStore.first
+            }
+
+            coEvery { getRefreshToken() } answers {
+                tokenStore.second
+            }
+
+            coEvery { authorized() } answers {
+                tokenStore.first.isNotBlank() && tokenStore.second.isNotBlank()
+            }
+        }
+
+        dataRepository = DataRepositoryImpl(retrofitMockRepository, persistenceMockRepository)
     }
 
     private suspend fun registerTestUserWithRole(
         role: UserRole, email: String = "test@debug.mail", password: String = "password"
     ) {
-        val result = dataMockRepository.register(
+        val result = dataRepository.register(
             firstname = "TestUser",
             lastname = "Lastname",
             patronymic = "Patronymic",
             phoneNumber = "+7999999999",
             email = email,
             password = password,
+            role = role,
+            autologin = false,
+            retried = false,
+        )
+
+        assert(result.isSuccessful)
+    }
+
+    private fun addCapturedUserToListAndRetrieve(role: UserRole): MockUserModel {
+        val user = MockUserModel(
+            email = registerRequestSlot.captured.email,
+            password = registerRequestSlot.captured.password,
             role = role
         )
 
-        assert(result.second.isSuccess)
-    }
-
-    private fun addCapturedUserToList(role: UserRole) {
-        usersList.add(
-            MockUserModel(
-                email = registerRequestSlot.captured.email,
-                password = registerRequestSlot.captured.password,
-                role = role
-            )
-        )
+        usersList.add(user)
+        return user
     }
 
     private suspend fun assertAuthSuccess(email: String, password: String) {
-        dataMockRepository.authenticate(email, password).apply {
-            assert(this.second.isSuccess) {"Expected success authentication for $email:$password; got failure."}
+        dataRepository.authenticate(email, password).apply {
+            assert(this.isSuccessful) { "Expected success authentication for $email:$password; got failure." }
         }
     }
 
     private suspend fun assertAuthFailure(email: String, password: String) {
-        dataMockRepository.authenticate(email, password).apply {
-            assert(this.second.isFailure) {"Expected failed authentication for $email:$password; got success."}
+        dataRepository.authenticate(email, password).apply {
+            assert(!this.isSuccessful) { "Expected failed authentication for $email:$password; got success." }
         }
     }
 
     @Test
     fun `Ping Test`() = runBlocking {
         assertEquals(
-            dataMockRepository.ping().getOrNull(), exceptedPingResponse
+            dataRepository.ping().rawData.getOrNull(), exceptedPingResponse
         )
     }
 
@@ -139,7 +209,7 @@ class DataRepositoryTest {
     fun `Register Coordinator Test`() = runBlocking {
         registerTestUserWithRole(UserRole.COORDINATOR)
 
-        coVerify(exactly = 1) { retrofitMockRepository.registerCoordinator(any()) }
+        coVerify(exactly = 1) { retrofitMockRepository.registerCoordinator("", any()) }
         assertEquals(usersList.size, 1)
     }
 
@@ -147,7 +217,7 @@ class DataRepositoryTest {
     fun `Register Admin Test`() = runBlocking {
         registerTestUserWithRole(UserRole.ADMIN)
 
-        coVerify(exactly = 1) { retrofitMockRepository.registerAdmin(any()) }
+        coVerify(exactly = 1) { retrofitMockRepository.registerAdmin("", any()) }
         assertEquals(usersList.size, 1)
     }
 
@@ -164,5 +234,29 @@ class DataRepositoryTest {
         assertAuthSuccess("admin@debug.mail", "admin")
 
         assertAuthFailure("volunteer@debug.mail", "wrong")
+    }
+
+    @Test
+    fun `Access & Refresh Tokens Being Set On Login Test`() = runBlocking {
+        registerTestUserWithRole(UserRole.VOLUNTEER, "volunteer@debug.mail", "volunteer")
+        assertAuthSuccess("volunteer@debug.mail", "volunteer")
+
+        assert(dataRepository.authorized())
+    }
+
+    @Test
+    fun `Access & Refresh Tokens Not Being Set On Failed Login Test`() = runBlocking {
+        registerTestUserWithRole(UserRole.VOLUNTEER, "volunteer@debug.mail", "volunteer")
+        assertAuthFailure("volunteer@debug.mail", "wrong")
+
+        assert(!dataRepository.authorized())
+    }
+
+    @Test
+    fun `Request New Access Token Succeeds Test`() = runBlocking {
+        registerTestUserWithRole(UserRole.VOLUNTEER, "volunteer@debug.mail", "volunteer")
+        assertAuthSuccess("volunteer@debug.mail", "volunteer")
+
+        assert(dataRepository.requestFreshAccessToken().isSuccessful)
     }
 }
