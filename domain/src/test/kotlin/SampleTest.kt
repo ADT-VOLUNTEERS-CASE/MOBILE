80b4d853c5673fa63b8090d0d20e51a4733380f8
freeze2222 <opop1094@gmail.com>
2026-04-16 19:59:01 +0300

    Add implementation unit coverage test into domain project

---
+++ domain/src/test/kotlin/SampleTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test
+
+class SampleTest {
+    @Test
+    //@AssociatedWith(DomainRepositoryImpl::class, DomainRepositoryImpl.SAMPLE)
+    fun samplePingTest(){
+
+    }
+}
