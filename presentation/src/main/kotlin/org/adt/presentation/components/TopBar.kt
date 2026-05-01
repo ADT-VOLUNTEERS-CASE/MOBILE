c65cc133b4184b55fceec53613eee3fca496139a
freeze2222 <opop1094@gmail.com>
2026-05-01 13:43:32 +0300

    Add weights to top bar sample

--- presentation/src/main/kotlin/org/adt/presentation/components/TopBar.kt
+++ presentation/src/main/kotlin/org/adt/presentation/components/TopBar.kt
@@ -1,6 +1,7 @@
 package org.adt.presentation.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -8,17 +9,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.outlined.MenuOpen
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import org.adt.presentation.R
 import org.adt.presentation.theme.VolunteersCaseTheme
 
 @Composable
@@ -31,20 +31,24 @@ fun TopBar(onDrawerToggleAction: () -> Unit= {}) {
             .padding(15.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        IconButton(
-            modifier = Modifier
-                .weight(4f)
-                .size(24.dp),
-            shape = CircleShape,
-            onClick = {
-                onDrawerToggleAction.invoke()
-            }) {
-            Icon(
-                imageVector = Icons.Default.ArrowBack,
-                contentDescription = "Open side menu",
-                modifier = Modifier.size(24.dp),
-                tint = Color.White
-            )
+        Box(modifier = Modifier.weight(1f)) {
+            IconButton(
+                modifier = Modifier
+                    .size(24.dp),
+                shape = CircleShape,
+                onClick = {
+                    onDrawerToggleAction.invoke()
+                }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Outlined.MenuOpen,
+                    contentDescription = "Open side menu",
+                    modifier = Modifier.size(24.dp).rotate(180f),
+                    tint = Color.White
+                )
+            }
+        }
+        Box(modifier = Modifier.weight(6f)){
+
         }
     }
 }
