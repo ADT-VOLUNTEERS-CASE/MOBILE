ffd698ad40d436506bc8d87332f6a009ee26e284
freeze2222 <opop1094@gmail.com>
2026-05-09 12:16:46 +0300

    Add vertical scroll to SearchOverlay's column

--- presentation/src/main/kotlin/org/adt/presentation/screens/home/volunteer/search/SearchOverlay.kt
+++ presentation/src/main/kotlin/org/adt/presentation/screens/home/volunteer/search/SearchOverlay.kt
@@ -15,7 +15,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -85,7 +87,9 @@ fun SearchOverlay(
                     color = Mint
                 )
             } else if (uiState.searchModeListEvent.isNotEmpty() || uiState.searchModeListLocation.isNotEmpty()) {
-                Column(Modifier.fillMaxWidth()) {
+                Column(Modifier
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState())) {
                     if (uiState.searchModeListEvent.isNotEmpty()) {
                         Text(
                             "Мероприятия",
