fd7c8c7d072e60d45724407452541a9c48057f4b
freeze2222 <opop1094@gmail.com>
2026-05-14 17:55:12 +0300

    Add pager indicator to RecommendationsRow

--- presentation/src/main/kotlin/org/adt/presentation/components/RecommendationsRow.kt
+++ presentation/src/main/kotlin/org/adt/presentation/components/RecommendationsRow.kt
@@ -1,5 +1,6 @@
 package org.adt.presentation.components
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,8 +10,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -20,16 +21,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.tbuonomo.viewpagerdotsindicator.compose.DotsIndicator
+import com.tbuonomo.viewpagerdotsindicator.compose.model.DotGraphic
+import com.tbuonomo.viewpagerdotsindicator.compose.type.ShiftIndicatorType
 import org.adt.core.entities.event.Event
 import org.adt.presentation.components.cards.RecommendationCard
 import org.adt.presentation.theme.VolunteersCaseTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun RecommendationsRow(
     modifier: Modifier = Modifier,
     events: List<Event> = listOf(Event(), Event())
 ) {
+    val pagerState = rememberPagerState(pageCount = { events.size })
+
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -41,22 +47,38 @@ fun RecommendationsRow(
         verticalArrangement = Arrangement.Center
     ) {
         Spacer(modifier = Modifier.height(16.dp))
+
         Text(
-            "Рекомендации для Вас:",
+            text = "Рекомендации для Вас:",
             style = VolunteersCaseTheme.typography.titleLarge
         )
-        LazyRow(
-            modifier = modifier
+
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
-                .padding(top = 16.dp, bottom = 16.dp),
-
-            contentPadding = PaddingValues(horizontal = 16.dp)
-        ) {
-            items(events) { event ->
-                RecommendationCard(modifier = Modifier, event = event)
-            }
+                .padding(top = 16.dp, bottom = 12.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp),
+            pageSpacing = 16.dp
+        ) { page ->
+            RecommendationCard(
+                modifier = Modifier.fillMaxWidth(),
+                event = events[page]
+            )
         }
+
+        DotsIndicator(
+            dotCount = events.size,
+            pagerState = pagerState,
+            modifier = Modifier.padding(bottom = 16.dp),
+            type = ShiftIndicatorType(
+                dotsGraphic = DotGraphic(
+                    color = VolunteersCaseTheme.colors.primaryBackground,
+                    size = 8.dp
+                )
+            )
+        )
     }
 }
 
