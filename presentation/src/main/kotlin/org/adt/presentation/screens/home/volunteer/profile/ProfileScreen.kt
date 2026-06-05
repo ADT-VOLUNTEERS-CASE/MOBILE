d51e5f389f9a938e4d4a826b40d2e94e5520394b
freeze2222 <opop1094@gmail.com>
2026-06-05 20:04:34 +0300

    Fix incorrect version display in volunteer profile screen Raise app version to 0.1.1 in build.gradle

--- presentation/src/main/kotlin/org/adt/presentation/screens/home/volunteer/profile/ProfileScreen.kt
+++ presentation/src/main/kotlin/org/adt/presentation/screens/home/volunteer/profile/ProfileScreen.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import org.adt.presentation.BuildConfig
 import org.adt.presentation.components.cards.ProfileMenuCard
 import org.adt.presentation.components.misc.NotImplementedSheet
 import org.adt.presentation.navigation.Destinations
@@ -215,7 +216,7 @@ fun ProfileScreenContent(
             Spacer(Modifier.height(32.dp))
 
             Text(
-                text = "Версия 0.1.1",
+                text = "Версия ${BuildConfig.VERSION_NAME}",
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(bottom = 24.dp),
