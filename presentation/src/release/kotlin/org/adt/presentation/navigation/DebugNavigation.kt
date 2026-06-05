f7adaecafba84eeb5d3de933081b8173def2cd12
freeze2222 <opop1094@gmail.com>
2026-06-05 14:37:17 +0300

    Fix debugGraph signature misalign between release and debug source sets

--- presentation/src/release/kotlin/org/adt/presentation/navigation/DebugNavigation.kt
+++ presentation/src/release/kotlin/org/adt/presentation/navigation/DebugNavigation.kt
@@ -1,6 +1,10 @@
 package org.adt.presentation.navigation
 
-// Ignore in release(is overridden in debug source set)
+import androidx.navigation.NavHostController
+
+// Ignore in release(overridden in debug source set)
+@Suppress("unused")
+context(navController: NavHostController)
 fun debugGraph() {
     // Empty
 }
